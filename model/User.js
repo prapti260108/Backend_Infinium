c1b23988a2c2dfe0a3989dc03c52a72ec1c609fd
prapti260108 <praptipatel260108@gmail.com>
2025-05-16 17:05:13 +0530

    completed

--- model/User.js
+++ model/User.js
@@ -7,4 +7,4 @@ const userSchema = new mongoose.Schema({
   isVerified: { type: Boolean, default: false }
 });
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
